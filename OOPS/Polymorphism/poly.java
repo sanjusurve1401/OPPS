996ac1ca25a5006f5bae0576f4e14c00f4c62ee7
sanjusurve1401 <105294442+sanjusurve1401@users.noreply.github.com>
2024-02-09 17:58:14 +0530

    Update poly.java

--- OOPS/Polymorphism/poly.java
+++ OOPS/Polymorphism/poly.java
@@ -1,59 +1,84 @@
+/*
+ * Static polymorphism.
+ * here we used the same name of method but different parameter in it and it also known as compile time polymorphism
+ * We implement it on the computefees method 
+ * we create 2 method with same name but with different parameters
+ * 
+ * public float computeFees()
+ * public float computeFees(int annualFees)`
+ * 
+ * we can see the difference in the parameteres but not in the name the type of variable using 
+ * 
+ * Compiler understand when to invoked which method
+ * 
+ * 
+ * System.out.println(s1.getDetails()); FOr this statement
+ * public float computeFees()           Compiler uses this method
+ * 
+ * System.out.println(s3.computeFees(500000)); FOr this statement
+ * public float computeFees(int annualFees)    Compiler uses this method
+ */
+
+
+public class poly1{
+    public static void main(String[] args){
+        Student s1 = new Student("SANJEET", 2);
+        System.out.println(s1.getDetails());
+
+        ResearchStudent s2 = new ResearchStudent("Mihika", 1, "IOT");
+        System.out.println(s2.getDetails());
+        System.out.println(s2.computeFees(100000));
+
+        PhD s3 = new PhD("Vanshika", 3, "Software", "FSD");
+        System.out.println(s3.getDetails());
+        System.out.println(s3.computeFees(500000));
+    }
+}
 class Student{
     private final String name;
     private int year;
     private static final int annualFees = 10000;
 
-    public Student(String Name, int Year){
-        this.name = Name;
-        this.year = Year;
+    public Student(String name, int year){
+        this.name = name;
+        this.year = year;
     }
 
     public float computeFees(){
         return Student.annualFees * this.year;
     }
+    
+    public float computeFees(int annualFees){
+        return annualFees * this.year;
+    }
 
     public String getDetails(){
         return "\nName: "+this.name+
-        "\nYear: "+ this.year+
-        "\nFees: "+this.computeFees();
+        "\nYear: "+this.year+
+        "\nCompute Fees"+computeFees();
     }
 }
-class researchStudent extends Student{
-    private String researchArea;
+class ResearchStudent extends Student{
+    private String ResearchArea;
 
-    public researchStudent(String Name, int Year, String ResearchArea) {
-        super(Name, Year);
-        this.researchArea = ResearchArea;
+    public ResearchStudent(String name, int year, String ResearchArea) {
+        super(name, year);
+        this.ResearchArea = ResearchArea;
     }
 
     public String getDetails(){
-        return super.getDetails()+ "\nResearch Area: "+this.researchArea;
+        return super.getDetails() + "\nResearch Area: "+this.ResearchArea;
     }
-    
 }
-class PhDStudent extends researchStudent{
+class PhD extends ResearchStudent{
     private String Thesis;
 
-    public PhDStudent(String Name, int Year, String researchArea, String thesis) {
-        super(Name, Year,researchArea);
-        this.Thesis = thesis;
+    public PhD(String name, int year, String ResearchArea, String Thesis) {
+        super(name, year, ResearchArea);
+        this.Thesis = Thesis;
     }
 
     public String getDetails(){
-        return super.getDetails()+ "\nThesis Topic: "+this.Thesis;
-    }
-    
-}
-
-
-public class poly{
-    public static void main(String[] args) {
-        Student s1 = new Student("Sanjeet", 1);
-        researchStudent s2 = new researchStudent("Vanshika", 2, "IOT");
-        PhDStudent s3 = new PhDStudent("Mihika", 4, "SoftDev", "WEB DEV");
-
-        s1.getDetails();
-        s2.getDetails();
-        s3.getDetails();
+        return super.getDetails()+ "\nThesis: " + this.Thesis;
     }
 }
